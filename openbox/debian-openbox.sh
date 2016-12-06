d9e15f1d64678d5e2c0e21f266ef4b777ca7f086
gnew <gnew@gnewnet.homework>
2016-12-06 21:42:51 -0200

    all

--- openbox/debian-openbox.sh
+++ openbox/debian-openbox.sh
@@ -25,10 +25,19 @@ cp ~/comandos/openbox/70-synaptics.conf /usr/share/X11/xorg.conf.d/.
 #-- OPENBOX INSTALL
 apt-get install openbox openbox-menu compton slim tint2 pcmanfm gpicview conky -y
 
+#-- NETWORK INSTALL
+apt-get install network-manager network-manager-gnome -y
+
+#-- LXTERMINAL
+apt-get install lxterminal -y
+
 #-- CONFIG OPENBOX
 mkdir /home/$LOGIN/Imagens
 mkdir /home/$LOGIN/.config
 mkdir /home/$LOGIN/.config/tint2
+mkdir /home/$LOGIN/.fonts
+mkdir /home/$LOGIN/.config/pcmanfm
+mkdir /home/$LOGIN/.config/pcmanfm/default
 cp ~/comandos/openbox/bg.jpg /home/$LOGIN/Imagens/.
 cp ~/comandos/openbox/compton.conf /home/$LOGIN/.compton.conf
 cp ~/comandos/openbox/xinitrc /home/$LOGIN/.xinitrc
@@ -38,4 +47,7 @@ mkdir /home/$LOGIN/.config/openbox
 cp ~/comandos/openbox/configs/* /home/$LOGIN/.config/openbox/.
 cp -r ~/comandos/openbox/archlinux-simplyblack /usr/share/slim/themes/
 cp ~/comandos/openbox/conkyrc /home/$LOGIN/.conkyrc
+cp ~/comandos/openbox/fonts/* /home/$LOGIN/.fonts/.
+cp ~/comandos/openbox/desktop-items-0.conf /home/$LOGIN/.config/pcmanfm/default/.
+cp ~/comandos/openbox/pcmanfm.conf /home/$LOGIN/.config/pcmanfm/default/.
 chown -R gnew.gnew /home/$LOGIN/
